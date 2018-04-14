52714ac354f9026c39a51028b6849a0f51f5314e
piotrek@lifeinmobile.com <z4YgbDEkuAYDLYsLhdGz>
2018-04-14 21:31:55 +0200

    ok

--- build/Debug/MinGW7-Windows/Layer.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/MinGW7-Windows/Layer.o: Layer.cpp Layer.h LayerType.h \
- LayerMatrix.h
-
-Layer.h:
-
-LayerType.h:
-
-LayerMatrix.h:
--- build/Debug/MinGW7-Windows/LayerMatrix.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/MinGW7-Windows/LayerMatrix.o: LayerMatrix.cpp LayerMatrix.h
-
-LayerMatrix.h:
